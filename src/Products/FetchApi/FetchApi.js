6ad93ba5d8442c886ba360d1eecacefd6cd09b74
fafiktheprogramist <glowaczra@gmail.com>
2023-11-08 23:29:14 +0100

    nie wiem jak, ale dziala

--- src/Products/FetchApi/FetchApi.js
+++ src/Products/FetchApi/FetchApi.js
@@ -1,28 +1,43 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect } from 'react'
+import { useParams  } from 'react-router-dom'
 import Product from '../Product/Product'
 
 const FetchApi = () => {
-
-  const [data, setData] = useState([])
+  const { id } = useParams()
+  const [product, setProduct] = useState(null)
+  const [data , setData] = useState([])
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('https://localhost:7162/api/products')
-      const data = await response.json()
-      setData(data)
+      if(!id) {
+        const response = await fetch(`https://localhost:7162/api/products`)
+        const data = await response.json()
+        setData(data)
+    } else {
+        const response = await fetch(`https://localhost:7162/api/products/${id}`)
+        const data = await response.json()
+        setProduct(data)
+      }
     }
 
     fetchData()
-  }, [])
+  }, [id])
 
-return (<div>
+  return (
+    <div>
       <h1>FetchApi</h1>
-      {data && data.map((item) => <Product product={item} key={item.id} />)}
-      {/* <Product product={data} />
-      {console.log(data)}
-      {console.log(data.length)} */}
-      {/* TO DO trzeba dodać obsługę pojedynczego jsona, bo przychodzi tablica jsonów a w przypadku jednego nie ma tablicy */}
-    </div>)
-  }
-
+      {id ? (
+        product ? (
+          <Product product={product} />
+        ) : (
+          <p>Loading...</p>
+        )
+      ) : (
+        data.map((item) => (
+          <Product key={item.id} product={item} />
+        ))
+      )}
+    </div>
+  );
+}
 export default FetchApi;
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./HomePage";
+import Homepage from "./HomePage";import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import NotFound from "./NotFound";
 import Basket from "./Basket";
 import Products from "./Products";
-
+import FetchApi from "../Products/FetchApi/FetchApi";
 
 const RouterReact = () => (
   <div>
@@ -11,7 +11,8 @@ const RouterReact = () => (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/basket" element={<Basket />} />
-      <Route path="/products/*" element={<Products />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/:id" component={FetchApi} element={<Products />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>

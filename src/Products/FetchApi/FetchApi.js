3b91aabb2f5e4c21ca182460e440832afe23342d
glabojko <glabojko@gmail.com>
2023-11-10 14:46:45 +0100

    /api/products/byCategory fetched

--- src/Products/FetchApi/FetchApi.js
+++ src/Products/FetchApi/FetchApi.js
@@ -1,43 +1,25 @@
-import React, {useState, useEffect } from 'react'
-import { useParams  } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
 import Product from '../Product/Product'
 
 const FetchApi = () => {
-  const { id } = useParams()
-  const [product, setProduct] = useState(null)
+
   const [data, setData] = useState([])
 
   useEffect(() => {
     async function fetchData() {
-      if(!id) {
-        const response = await fetch(`https://localhost:7162/api/products`)
-        const data = await response.json()
-        setData(data)
-    } else {
-        const response = await fetch(`https://localhost:7162/api/products/${id}`)
-        const data = await response.json()
-        setProduct(data)
-      }
+      const response = await fetch('https://localhost:7162/api/products/byCategory/1')
+      const data = await response.json()
+      setData(data)
     }
 
     fetchData()
-  }, [id])
+  }, [])
 
-  return (
-    <div>
+return (<div>
       <h1>FetchApi</h1>
-      {id ? (
-        product ? (
-          <Product product={product} />
-        ) : (
-          <p>Loading...</p>
-        )
-      ) : (
-        data.map((item) => (
-          <Product key={item.id} product={item} />
-        ))
-      )}
-    </div>
-  );
-}
+      {data && data.map((item) => <Product product={item} key={item.id} />)}
+
+    </div>)
+  }
+
 export default FetchApi;

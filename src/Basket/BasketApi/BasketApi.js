6b834ee19c7268b1501c4d4703bd98398bcbcae7
RadekRo <r.roclawski@ankara.pl>
2023-11-19 09:42:13 +0100

    Added fetchbasketdata active on basket open to fetch newest basket status

--- src/Basket/BasketApi/BasketApi.js
+++ src/Basket/BasketApi/BasketApi.js
@@ -1,19 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import Basket from '../Item/Item'
 
-const BasketsApi = props => {
+const BasketsApi = (props) => {
+  const [data, setData] = useState([]);
 
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    async function fetchData() { 
-        const response = await fetch(`https://localhost:7162/api/basket/${props.clientId}`)
-        const data = await response.json()
-        setData(data)
+  const fetchBasketData = async () => {
+    
+    try {
+      const response = await fetch(`https://localhost:7162/api/basket/${props.clientId}`);
+      const newData = await response.json();
+      setData(newData);
+    } catch (error) {
+      console.error('Error fetching basket data:', error);
     }
+  };
 
-    fetchData()
-  }, [props.clientId])
+  useEffect(() => { fetchBasketData(); });
 
   return (
     <div>

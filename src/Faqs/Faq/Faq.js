4ab8b0da011b633a8b7ef57f83f5c3d8cf15fb50
glabojko <glabojko@gmail.com>
2023-11-21 19:46:34 +0100

    minor changes in Faqs css

--- src/Faqs/Faq/Faq.js
+++ src/Faqs/Faq/Faq.js
@@ -2,7 +2,7 @@ import "./Faq.css";
 
 const Faq = (props) => (
   <div>
-    <div>Faq nr: <b>{props.faq.id}</b></div>
+    <div className="answer">Faq nr: <b>{props.faq.id}</b></div>
     <div className="question"><b>Pytanie: </b>{props.faq.question}</div>
     <div className="answer"><b>Odpowiedź:</b> {props.faq.answer}</div>
     <div><br></br></div>

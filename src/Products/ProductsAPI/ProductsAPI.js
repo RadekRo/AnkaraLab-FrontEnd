7debd2f97c9e7d76286fe99e7b57f9a7ecace468
RadekRo <r.roclawski@ankara.pl>
2023-11-22 13:29:42 +0100

    fea: added heading with categories to products

--- src/Products/ProductsAPI/ProductsAPI.js
+++ src/Products/ProductsAPI/ProductsAPI.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Product from '../Product/Product';
 
 const ProductsAPI = props => {
@@ -39,6 +40,14 @@ const ProductsAPI = props => {
       <Container>
         <Row>
           <Col className="fs-2 bg-success text-white pt-1 pb-1 rounded">{theme}</Col>
+        </Row>
+        <Row className="border-bottom pt-2 pb-2">
+          <Col>Kategorie &rarr;</Col>
+          <Col><Link className="text-decoration-none" to = {`../products/1`}>Odbitki fotograficzne</Link></Col>
+          <Col><Link className="text-decoration-none" to = {`../products/2`}>Wielki format</Link></Col>
+          <Col><Link className="text-decoration-none" to = {`../products/5`}>Fotoobrazy</Link></Col>
+          <Col><Link className="text-decoration-none" to = {`../products/6`}>Wydruki magnetyczne</Link></Col>
+          <Col><Link className="text-decoration-none" to = {`../products/3`}>Fotogadżety</Link></Col>
         </Row>
         {data && data.map((item) => <Product product={item} key={item.id} clientId={props.clientId} />)}
       </Container>)

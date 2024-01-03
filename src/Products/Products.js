f0f01a931609579cb20883b9bdf5791d2a0d9085
Beata Piekarek <be.piekarek@gmail.com>
2024-01-03 20:45:39 +0100

    feat: sessionStorage impemented

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 import papers from "../TempData/PaperData";
 import crops from "../TempData/CropData";
 import "./Products.css";
-
+import Basket from "../Basket/Basket";
 
 const Products = () => {
   // pobranie parametrów z adresu w przegladarce
@@ -23,54 +23,96 @@ const Products = () => {
       const categoryIdString = crop.categoryId.toString();
       const idString = id.toString();
       return categoryIdString.includes(idString);
-    })
-  }
+    });
+  };
   // symulacja fetcha
   const filteredProducts = getProductByCategory(categoryId);
   const filteredPapers = getPapersByCategory(categoryId);
   const filteredCrops = getCropsByCategory(categoryId);
 
-  const [selectedSize, setSelectedSize]=useState(filteredProducts.find(product=>product.isDefault));
-  const [selectedPaper, setSelectedPaper]=useState(filteredPapers.find(paper=>paper.isDefault));
-  const [selectedCrop, setSelectedCrop]=useState(filteredCrops.find(crop=>crop.isDefault));
-  console.log(selectedCrop)
+  const [selectedSize, setSelectedSize] = useState(
+    filteredProducts.find((product) => product.isDefault)
+  );
+  const [selectedPaper, setSelectedPaper] = useState(
+    filteredPapers.find((paper) => paper.isDefault)
+  );
+  const [selectedCrop, setSelectedCrop] = useState(
+    filteredCrops.find((crop) => crop.isDefault)
+  );
 
-  const handleSizeChange = (event) => {setSelectedSize(filteredProducts[event.target.value -1 ])}
-  const handlePaperChange = (event) => {setSelectedPaper(filteredPapers[event.target.value -1])}
-  const handleCropChange = (event) => {setSelectedCrop(filteredCrops[event.target.value -1])}
+  const basketItem = {
+    size: selectedSize.name,
+    paper: selectedPaper.name,
+    crop: selectedCrop.name,
+  };
+
+  const handleSizeChange = (event) => {
+    setSelectedSize(filteredProducts[event.target.value - 1]);
+  };
+  const handlePaperChange = (event) => {
+    setSelectedPaper(filteredPapers[event.target.value - 1]);
+  };
+  const handleCropChange = (event) => {
+    setSelectedCrop(filteredCrops[event.target.value - 1]);
+  };
+  const AddToABasket = () => {
+    sessionStorage.setItem("Basket", JSON.stringify(basketItem));
+  };
+  console.log(JSON.parse(sessionStorage.getItem("Basket")));
   return (
     <div className="text-center">
       <Link to="/" className="btn btn-sn btn-info p-1">
         HomePage
       </Link>
       <h1>Products</h1>
-    <form className="form-product">
-      <label htmlFor="odbitki">Wybierz produkt:</label>
-      <select className="form-select select-color" id="produkt" name="size" value={selectedSize.id} onChange={handleSizeChange}>
-        {filteredProducts.map((product) => (
-          <option key={product.id} value={product.id}>
-            {product.name}
-          </option>
-        ))}
-      </select>
-      <br />
-      <label htmlFor="papier">Wybierz papier:</label>
-      <select className="form-select select-color" id="papier" name="papier" value={selectedPaper.id} onChange={handlePaperChange}>
-        {filteredPapers.map((paper) => (
-          <option key={paper.id} value={paper.id}>
-            {paper.name}
-          </option>
-        ))}
-      </select>
-      <br />
-      <label htmlFor="crop">Wybierz kadrowanie:</label>
-      <select className="form-select select-color" id="crop" name="crop" value={selectedCrop.id} onChange={handleCropChange}>
-        {filteredCrops.map((crop) => (
-          <option key={crop.id} value={crop.id}>
-            {crop.name}
-          </option>
-        ))}
-      </select>
+      <form className="form-product">
+        <label htmlFor="odbitki">Wybierz produkt:</label>
+        <select
+          className="form-select select-color"
+          id="produkt"
+          name="size"
+          value={selectedSize.id}
+          onChange={handleSizeChange}
+        >
+          {filteredProducts.map((product) => (
+            <option key={product.id} value={product.id}>
+              {product.name}
+            </option>
+          ))}
+        </select>
+        <br />
+        <label htmlFor="papier">Wybierz papier:</label>
+        <select
+          className="form-select select-color"
+          id="papier"
+          name="papier"
+          value={selectedPaper.id}
+          onChange={handlePaperChange}
+        >
+          {filteredPapers.map((paper) => (
+            <option key={paper.id} value={paper.id}>
+              {paper.name}
+            </option>
+          ))}
+        </select>
+        <br />
+        <label htmlFor="crop">Wybierz kadrowanie:</label>
+        <select
+          className="form-select select-color"
+          id="crop"
+          name="crop"
+          value={selectedCrop.id}
+          onChange={handleCropChange}
+        >
+          {filteredCrops.map((crop) => (
+            <option key={crop.id} value={crop.id}>
+              {crop.name}
+            </option>
+          ))}
+        </select>
+        <div className="btn btn-info p-1" onClick={AddToABasket}>
+          dawaj baskjet
+        </div>
       </form>
     </div>
   );

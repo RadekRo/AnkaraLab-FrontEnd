1a276e53dc2eb8062cccb1227cc7b2f508b229ab
glabojko <glabojko@gmail.com>
2024-01-04 20:43:51 +0100

    FEAT: basket styling added

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./Basket.css";
+import { Row, Col } from "react-bootstrap";
 
 const Basket = () => {
   const [storedBasket, setStoredBasket] = useState([]);
 
 useEffect(() => {
   const currentBasket = JSON.parse(sessionStorage.getItem("Basket"));
-  setStoredBasket(currentBasket);
+  if (currentBasket) {
+  setStoredBasket(currentBasket);}
 }, []);
   
   const removeFromBasket = (itemIndex) => {
@@ -18,16 +21,20 @@ useEffect(() => {
   return (
     <div>
   <Link to="/" className="btn btn-info">&larr; Home</Link>
-  <div>Basket</div>
-  <div>{storedBasket.map((basketItem, index) => (
-    <div key = {index} className = "d-flex">
-      <p>{index + 1} </p>
-      <p>{basketItem.size}</p>
-      <p>{basketItem.paper}</p>
-      <p>{basketItem.crop}</p>
-      <div className = "btn btn-danger" onClick={() => removeFromBasket(index)}>Usuń</div>
-    </div>
+  <div>Basket logo</div>
+  {storedBasket.length < 1 ? (
+    <div>Twój koszyk jest pusty</div>
+  ) : (
+  <div className="form-basket">{storedBasket.map((basketItem, index) => (
+    <Row className="m-1 border-bottom p-1 pb-2" key = {index}>
+      <Col>{index + 1} </Col>
+      <Col className="col-6 d-flex justify-items-left">{basketItem.size}</Col>
+      <Col className="col-2">{basketItem.paper}</Col>
+      <Col className="col-2">{basketItem.crop}</Col>
+      <Col className="col-1"><div className = "btn btn-sm btn-danger" onClick={() => removeFromBasket(index)}>Usuń</div></Col>
+    </Row>
   ))}</div>
+  )}
   </div>
 )};
 export default Basket;

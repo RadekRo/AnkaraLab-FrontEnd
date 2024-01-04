839b2ff62526fbb115843a32b1ae49cfb8f6fe42
glabojko <glabojko@gmail.com>
2024-01-04 20:17:48 +0100

    FEAT: removing items from basket

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -1,4 +1,33 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Basket = () => {
-  return <div>Baskjet</div>;
-};
+  const [storedBasket, setStoredBasket] = useState([]);
+
+useEffect(() => {
+  const currentBasket = JSON.parse(sessionStorage.getItem("Basket"));
+  setStoredBasket(currentBasket);
+}, []);
+  
+  const removeFromBasket = (itemIndex) => {
+    const updatedBasket = storedBasket.filter((_, index) => index !== itemIndex);
+    setStoredBasket(updatedBasket);
+    sessionStorage.setItem("Basket", JSON.stringify(updatedBasket));
+  }
+
+  return (
+    <div>
+  <Link to="/" className="btn btn-info">&larr; Home</Link>
+  <div>Basket</div>
+  <div>{storedBasket.map((basketItem, index) => (
+    <div key = {index} className = "d-flex">
+      <p>{index + 1} </p>
+      <p>{basketItem.size}</p>
+      <p>{basketItem.paper}</p>
+      <p>{basketItem.crop}</p>
+      <div className = "btn btn-danger" onClick={() => removeFromBasket(index)}>Usuń</div>
+    </div>
+  ))}</div>
+  </div>
+)};
 export default Basket;

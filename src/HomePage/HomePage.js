e9721178c28443479c0966211fbf61bc3efc8700
RadekRo <r.roclawski@ankara.pl>
2024-01-07 18:54:50 +0100

    style: moved logo to header for visibility on every page. Logo linked to mainPage

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,9 +5,6 @@ console.log(categories);
 const HomePage = () => {
   return (
     <div>
-      <div>
-        <img src='/images/alabLogo.jpg' alt='logotyp' />
-      </div>
       {categories.map((category) => (
         <Link to={`/category/${category.id}`} key = {category.id} className="btn btn-warning btn-lg m-2">
           <div>{category.name}</div>
--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <Link to="/basket"> Baskjet</Link>
+    <div className='d-flex justify-content-between'>
+      <div>
+        <Link to="/"><img src='/images/alabLogo.jpg' alt='logotyp' /></Link>
+      </div>
+      <div>
+        <Link to="/basket"> Baskjet</Link>
+      </div>
     </div>
   );
 };

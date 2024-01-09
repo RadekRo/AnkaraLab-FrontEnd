706411f1fc0e21d4169ee663665c2c92cc7c1c8f
fafiktheprogramist <glowaczra@gmail.com>
2024-01-09 21:12:59 +0100

    refactor: cleaned up a little in code

--- src/Promotion/Promotion.js
+++ src/Promotion/Promotion.js
@@ -1,7 +1,6 @@
 import products from "../TempData/ProductData";
 import promotion from "../TempData/PromotionData";
 
-const promotionInterval = 7
 const Promotion = () => {
     const getPromoLvl = () => {
         return (Math.random() * (0.20 - 0.05) + 0.05).toFixed(2);
@@ -12,7 +11,6 @@ const Promotion = () => {
     }
     
     const getProductById = (id) => {
-        console.log(`${id} ${products.find(product => product.id === id)}`)
         return products.find(product => product.id === id);
     }
 
@@ -21,9 +19,9 @@ const Promotion = () => {
         let newPromotion = {
             id: promotion.id + 1,
             promotedItemId : getRandomPromoItemId(),
-            startDate : new Date(today), //DateTime.Now
-            expiryDate : new Date(today.setDate(today.getDate() +5)), // DateTime.Now.AddDays(7)
-            discountLvl :  getPromoLvl() // 0.05 - 0.2
+            startDate : new Date(today),
+            expiryDate : new Date(today.setDate(today.getDate() +7)),
+            discountLvl :  getPromoLvl()
         }
         return newPromotion;
     }

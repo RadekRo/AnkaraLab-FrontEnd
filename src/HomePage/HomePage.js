528507caf6b5e185a2d97322aa41ac47787e0bbf
RadekRo <r.roclawski@ankara.pl>
2024-01-26 22:40:44 +0100

    refactor: refactored login.js created separate loginform

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,13 +1,11 @@
 import Categories from "../Data/Categories";
 import Promotion from "../Promotion/Promotion";
-import UserDiscount from "../UserDiscount/UserDiscount";
 
 const HomePage = () => {
   return (
     <div>
       <Promotion />
       <Categories />
-      <UserDiscount />
     </div>
   );
 };

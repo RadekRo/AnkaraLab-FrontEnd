14e31cad4cb662907ed68eb28c32a5a7f5664e39
RadekRo <r.roclawski@ankara.pl>
2024-02-10 18:30:17 +0100

    style: styled newsletter

--- src/HomePage/Newsletter.js
+++ src/HomePage/Newsletter.js
@@ -1,10 +1,19 @@
 import './Newsletter.css';
 
+const emailValidation = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+const handleNewsletter = () => {
+  alert('Dziękujemy za zapisanie się do Newslettera!');
+};
+
 const Newsletter = () => {
   return (
     <div className="d-flex align-items-center justify-content-center Newsletter rounded p-2 text-dark">
-      Zapisz się do Newslettera: <input className='input rounded border-0 bg-light ms-3 me-3' placeholder='Twój e-mail'></input>
-      <button className='btn btn-sm btn-dark'>Zapisz</button>
+      Zapisz się do Newslettera: <input className='p-2 rounded border-0 bg-light ms-3 me-3' placeholder='Twój e-mail' />
+      <button className='btn btn-sm btn-dark' onClick={handleNewsletter}>Zapisz</button>
     </div>
   );
 };
